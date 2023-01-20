c2cb5d14fabf5a6cb788caa8b6e66c5beb20cc5c
Taylor <trmartin2@nic.edu>
2023-01-20 10:50:11 -0800

    cleaned up directory structure. Updated individual.py

---
+++ Code Examples/individual_ex.py
@@ -0,0 +1,84 @@
+import random #for random number generation
+import copy #for the copy function
+
+#set the size of the genome
+genome_size = 10
+
+#set the possible DNA characters
+DNA_Characters = ['A', 'C', 'G', 'T']
+
+class individual:
+   def __init__(self):
+
+      #Print Object Init Message
+      print("Creating A New Individual")
+
+      #init fitness to 0, we do not know the fitness of the individual yet.
+      self.fitness = 0
+
+      #init the genome to an empty list
+      self.genome = []
+
+      #populate the genome with random DNA characters
+      for i in range(0, genome_size):
+         self.genome.append(random.choice(DNA_Characters))
+
+      #calculate the fitness of the individual
+      self.calculate_fitness()
+
+   #print the individual function
+   def print_individual(self):
+
+      #print fitness and genome
+      print("Fitness: ", self.fitness)
+      print("Genome: ", self.genome)
+
+   #This is the fitness function for this problem.
+   #go through and add up all of the 'T' characters in the genome. 
+   #The total number of 'T' characters is the fitness of the individual.
+   def calculate_fitness(self):
+      #reset the fitness of the individual to 0
+      self.fitness = 0
+
+      #go through the genome and count the number of 'T' characters
+      for c in self.genome:
+         if c == 'T':
+            self.fitness += 1
+
+   #This is the mutation function for this problem.
+   #select a random index in the genome and change the character at that index to a random DNA character.
+   def mutation(self):
+
+      #select a random index in the genome
+      index = random.randint(0, genome_size - 1)
+
+      #select a random DNA character
+      new_char = random.choice(DNA_Characters)
+
+      #set the character at the random index to the random DNA character
+      self.genome[index] = new_char
+
+      #recalculate the fitness of the individual
+      self.calculate_fitness()
+
+
+def main():
+   
+   #mother
+   ind1 = individual()
+   ind1.print_individual()
+
+   #daughter
+   ind2 = individual()
+
+   #cell division by copying
+   ind2 = copy.deepcopy(ind1)
+
+   #print and mutate the daughter
+   ind2.print_individual()
+   ind2.mutation()
+   ind2.print_individual()
+
+
+if __name__ == "__main__":
+   main()
