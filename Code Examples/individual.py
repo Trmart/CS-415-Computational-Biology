ed31c6b1b7100a5a0a2aa0226eb02b66c73a1692
Taylor <mart8517@vandals.uidaho.edu>
2023-02-06 11:15:04 -0800

    updated calulate_fitness function to count triplets of Ts

--- Code Examples/individual.py
+++ Code Examples/individual.py
@@ -2,7 +2,7 @@ import random #for random number generation
 #import copy #for the copy function
 
 #set the size of the genome
-genome_size = 10
+genome_size = 50
 
 #set the possible DNA characters
 DNA_Characters = ['A', 'C', 'G', 'T']
@@ -37,17 +37,40 @@ class individual:
       #print the individual by calling the __str__ function
       print(self.__str__(), "\n")
 
-   """calculate_fitness function"""
+   """OLD calculate_fitness function"""
    """go through and add up all of the 'T' characters in the genome. """
    """The total number of 'T' characters is the fitness of the individual."""
+   # def calculate_fitness(self):
+   #    #reset the fitness of the individual to 0
+   #    self.fitness = 0
+
+   #    #go through the genome and count the number of 'T' characters
+   #    for c in self.genome:
+   #       if c == 'T':
+   #          self.fitness += 1
+
+   """calculate_fitness function"""
+   """go through and add up all of the 'T' character triplets in the genome. """
+   """The total number of 'T' character triplets is the fitness of the individual."""
    def calculate_fitness(self):
+      
       #reset the fitness of the individual to 0
       self.fitness = 0
-
-      #go through the genome and count the number of 'T' characters
-      for c in self.genome:
-         if c == 'T':
+      
+      #set the index to 0
+      i = 0
+      
+      #go through the genome and count the number of 'T' character triplets
+      while i < genome_size - 1:
+         #if the current character is a 'T' and the next two characters are 'T'
+         if self.genome[i] == 'T' and self.genome[i + 1] == 'T' and self.genome[i + 2] == 'T':
+            #add 1 to the fitness
             self.fitness += 1
+            #increment the index by 3
+            i += 2
+         #else no 'T' character triplets were found
+         #increment the index by 1
+         i += 1
 
    """mutation function"""
    """select a random index in the genome and change the character at that index to a random DNA character."""
