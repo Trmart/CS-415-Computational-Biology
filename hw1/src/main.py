5883194805c48e696863d3539c834f16cccf6a09
Taylor <mart8517@vandals.uidaho.edu>
2023-02-21 09:16:54 -0800

    completed experiment 1. Refactored and cleaned up code. Starting Experiment 2. Starting Project 1 Report

--- hw1/src/main.py
+++ hw1/src/main.py
@@ -5,85 +5,106 @@ import matplotlib.pyplot as plt
 """main function"""
 def main():
    """"""
-   #EXP1
-   #task 1
-   #have a set mutation rate of about 5-10% 
+   #********************EXP1**************************
+   
+   #Task 1
+   #have a set mutation rate of 5% 
    #plot the average fitness of the population over various population sizes
-   population_1 = population(50)
-   # population_1.print_population()
 
-   population_2 = population(100)
-   # population_2.print_population()
+   populations_1 = create_populations_of_increasing_magnitude(50, 10, 50)
 
-   population_3 = population(200)
-   # population_3.print_population()
+   mutate_population_lists(populations_1, 5)
+   
+   
+   mean_population_fitnesses1 = []
 
-   population_4 = population(500)
-   # population_4.print_population()
+   for index in range(0, len(populations_1)):
+      mean_population_fitnesses1.append(populations_1[index].avg_fitness)
+   
+   means_of_populations = {'means_of_population1' : mean_population_fitnesses1}
 
-   population_1.mutate_population(5)
-   population_2.mutate_population(5)
-   population_3.mutate_population(5)
-   population_4.mutate_population(5)
-
-   mean_population_fitnesses = [population_1.avg_fitness, population_2.avg_fitness, population_3.avg_fitness, population_4.avg_fitness]
-
-   plt.scatter([50, 100, 200, 500], mean_population_fitnesses)
+   plt.scatter([50, 100, 150, 200, 250, 300, 350, 400, 450, 500], mean_population_fitnesses1)
    plt.xlabel('Population Size')
    plt.ylabel('Average Fitness')
-   plt.title('Average Fitness vs Population Size')
+   plt.title('Average Fitness vs Population Size with 5% Mutation Rate')
    plt.show()
-   #task 2
-   #increase the mutation rate to about 20-25%
+   
+   
+   """Task 2"""
+
+   #increase the mutation rate to 25%
    #plot the average fitness of the population over various population sizes
-   population_1 = population(50)
-   # population_1.print_population()
 
-   population_2 = population(100)
-   # population_2.print_population()
+   populations_2 = create_populations_of_increasing_magnitude(50, 10, 50)
 
-   population_3 = population(200)
-   # population_3.print_population()
+   mutate_population_lists(populations_2, 25)
+   mean_population_fitnesses2 = []
 
-   population_4 = population(500)
-   # population_4.print_population()
-
-   population_1.mutate_population(25)
-   population_2.mutate_population(25)
-   population_3.mutate_population(25)
-   population_4.mutate_population(25)
-
-   mean_population_fitnesses = [population_1.avg_fitness, population_2.avg_fitness, population_3.avg_fitness, population_4.avg_fitness]
-
-   plt.scatter([50, 100, 200, 500], mean_population_fitnesses)
+   # mean_population_fitnesses = [population_1.avg_fitness, population_2.avg_fitness, population_3.avg_fitness, population_4.avg_fitness, population_5.avg_fitness, population_6.avg_fitness, population_7.avg_fitness, population_8.avg_fitness, population_9.avg_fitness, population_10.avg_fitness]
+   for index in range(0, len(populations_2)):
+      mean_population_fitnesses2.append(populations_2[index].avg_fitness)
+   
+   means_of_populations.update({'means_of_population2' : mean_population_fitnesses2})
+   
+   plt.scatter([50, 100, 150, 200, 250, 300, 350, 400, 450, 500], mean_population_fitnesses2)
    plt.xlabel('Population Size')
    plt.ylabel('Average Fitness')
-   plt.title('Average Fitness vs Population Size')
+   plt.title('Average Fitness vs Population Size with 25% Mutation Rate')
    plt.show()
 
-   #task 3
+   """Task 3"""
+
    #plot the average fitness of the population over various mutation rates and population sizes
+   plt.scatter([50, 100, 150, 200, 250, 300, 350, 400, 450,  500], means_of_populations['means_of_population1'], label = '5% Mutation Rate', color = 'red')
+   plt.scatter([50, 100, 150, 200, 250, 300, 350, 400, 450,  500], means_of_populations['means_of_population2'], label = '25% Mutation Rate', color = 'blue')
+   plt.xlabel('Population Size')
+   plt.ylabel('Average Fitness')
+   plt.title('Average Fitness vs Population Size with Various Mutation Rates')
+   plt.legend()
+   plt.show()
 
-   #task 4
+   """Task 4"""
    #Work on EXP1 part of Report
 
-   #EXP2
-   #task 1
+   
+   
+   #********************EXP2********************
+   
+   """Task 1"""
+
    #have an initial fitness function just matching "T" characters
    #plot the average fitness of the population over various population sizes
 
 
-   #task 2
+   """Task 2"""
+
    #implement a new fitness function that matches "T" characters in groups of 3
    #plot the average fitness of the population over various population sizes
 
-   #task 3
+   """Task 3"""
+
    #plot the average fitness of the population over various population sizes
 
-   #task 4
+   """Task 4"""
+
    #Work on EXP2 part of Report
 
 
+
+def create_populations_of_increasing_magnitude(initial_population_size, number_of_populations, magnitude_of_population_increase):
+   """create a population of a set magnitude"""
+   population_list = []
+
+   for i in range(0,number_of_populations):
+      population_list.append(population(initial_population_size))
+      initial_population_size += magnitude_of_population_increase
+   
+   return population_list
+
+def mutate_population_lists(population_list, mutation_rate):
+   """mutate a list of populations"""
+   for population in population_list:
+      population.mutate_population(mutation_rate)
 
 """call main"""
 if __name__ == "__main__":
